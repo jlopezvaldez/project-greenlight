b7c46fb690decc8b0db667453bb9c965df85149e
jonathan <jlopezv0324@gmail.com>
2023-04-12 15:13:30 -0700

    commit

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -24,7 +24,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	//If you don’t use a pointer, it will return a json.InvalidUnmarshalError error at runtime.
 	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
